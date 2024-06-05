48ecd037d0ffd919302599fc5a10090540117ef9
Natalija <vukosavljenatalija@gmail.com>
2024-06-05 22:50:25 +0200

    adding testing

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,13 @@
 """Test suite."""
+from fastapi.testclient import TestClient
 
+from mainapp.main import app
 
-
+# Create a Test Client for FastAPI app
+client = TestClient(app)
+# Test FastAPI using TestClient
+def test_root() -> None:
+    """Test main page."""
+    response = client.get("/")
+    assert response.status_code == 200  # noqa: S101, PLR2004
+    assert response.json() == {"message": "Hello World"}  # noqa: S101
